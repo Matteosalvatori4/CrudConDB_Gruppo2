dacd4ad1a76eaf8d9e578a289c7314ebac0e1f25
Matteo <matteo.salvatori4@gmail.com>
2021-03-12 23:25:38 +0100

    Creazione Progetto
    Creazione Progetto

---
+++ Crud-con-db-server/src/main/java/due/gruppo/Crudcondbserver/CrudConDbServerApplication.java
@@ -0,0 +1,13 @@
+package due.gruppo.Crudcondbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudConDbServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudConDbServerApplication.class, args);
+	}
+
+}
---
+++ Crud-con-db-server/src/test/java/due/gruppo/Crudcondbserver/CrudConDbServerApplicationTests.java
@@ -0,0 +1,13 @@
+package due.gruppo.Crudcondbserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudConDbServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
